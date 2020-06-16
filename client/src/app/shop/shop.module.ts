97054aad1715dcfe26a4d8bf516d4014ee52b6b0
Andy <shortzenhausen@gmail.com>
2020-06-16 19:06:31 +0100

    added routing

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,17 +4,16 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/share.module';
 import { PagingHeaderComponent } from '../shared/components/paging-header/paging-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ShopComponent,
-    PagingHeaderComponent
+    SharedModule,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
