29585b478a1db922cb4b29244e2e9521dc802ccf
Andy <shortzenhausen@gmail.com>
2020-06-19 19:22:43 +0100

    Added client checkout feature

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
   { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
-  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' }  ,
+  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
